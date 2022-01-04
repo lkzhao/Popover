01aea62cc8ddef2e6d82c614f19c2fea000a30d7
Luke Zhao <me@lkzhao.com>
2022-01-04 11:19:17 -0800

    make touch observe gesture publish

--- Sources/Popover/TouchObserveGesture.swift
+++ Sources/Popover/TouchObserveGesture.swift
@@ -1,9 +1,9 @@
 import UIKit
 
 /// a gesture that notifies the target whenever a touch begans. It doesn't block touch or any other gestures
-internal class TouchObserveGesture: UIGestureRecognizer {
+public class TouchObserveGesture: UIGestureRecognizer {
 
-  override init(target: Any?, action: Selector?) {
+  public override init(target: Any?, action: Selector?) {
     super.init(target: target, action: action)
     delaysTouchesBegan = false
     delaysTouchesEnded = false
@@ -11,26 +11,26 @@ internal class TouchObserveGesture: UIGestureRecognizer {
     delegate = self
   }
 
-  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+  public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
     super.touchesBegan(touches, with: event)
     state = .began
   }
 
-  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+  public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
     super.touchesMoved(touches, with: event)
     if state == .began {
       state = .changed
     }
   }
 
-  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+  public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
     super.touchesEnded(touches, with: event)
     if state == .began || state == .changed {
       state = .ended
     }
   }
 
-  override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+  public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
     super.touchesCancelled(touches, with: event)
     if state == .began || state == .changed {
       state = .cancelled
@@ -39,20 +39,20 @@ internal class TouchObserveGesture: UIGestureRecognizer {
 }
 
 extension TouchObserveGesture: UIGestureRecognizerDelegate {
-  func gestureRecognizer(
+  public func gestureRecognizer(
     _ gestureRecognizer: UIGestureRecognizer,
     shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
   ) -> Bool {
     return true
   }
 
-  func gestureRecognizer(
+  public func gestureRecognizer(
     _ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer
   ) -> Bool {
     return false
   }
 
-  func gestureRecognizer(
+  public func gestureRecognizer(
     _ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer
   ) -> Bool {
     return false
