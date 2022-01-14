639723cb16c01b06bc8d7c9b10cab41d44089f9c
Luke Zhao <me@lkzhao.com>
2022-01-14 15:21:21 -0800

    public gesture

--- Sources/Popover/TouchObserveGesture.swift
+++ Sources/Popover/TouchObserveGesture.swift
@@ -2,59 +2,65 @@ import UIKit
 
 /// a gesture that notifies the target whenever a touch begans. It doesn't block touch or any other gestures
 public class TouchObserveGesture: UIGestureRecognizer {
-
-  public override init(target: Any?, action: Selector?) {
-    super.init(target: target, action: action)
-    delaysTouchesBegan = false
-    delaysTouchesEnded = false
-    cancelsTouchesInView = false
-    delegate = self
-  }
-
-  public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
-    super.touchesBegan(touches, with: event)
-    state = .began
-  }
-
-  public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
-    super.touchesMoved(touches, with: event)
-    if state == .began {
-      state = .changed
+    
+    public override init(target: Any?, action: Selector?) {
+        super.init(target: target, action: action)
+        allowedTouchTypes = [
+            NSNumber(value: UITouch.TouchType.pencil.rawValue),
+            NSNumber(value: UITouch.TouchType.direct.rawValue),
+            NSNumber(value: UITouch.TouchType.indirect.rawValue),
+            NSNumber(value: UITouch.TouchType.indirectPointer.rawValue),
+        ]
+        delaysTouchesBegan = false
+        delaysTouchesEnded = false
+        cancelsTouchesInView = false
+        delegate = self
     }
-  }
-
-  public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
-    super.touchesEnded(touches, with: event)
-    if state == .began || state == .changed {
-      state = .ended
+    
+    public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesBegan(touches, with: event)
+        state = .began
     }
-  }
-
-  public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
-    super.touchesCancelled(touches, with: event)
-    if state == .began || state == .changed {
-      state = .cancelled
+    
+    public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesMoved(touches, with: event)
+        if state == .began {
+            state = .changed
+        }
     }
-  }
+    
+    public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesEnded(touches, with: event)
+        if state == .began || state == .changed {
+            state = .ended
+        }
+    }
+    
+    public override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent) {
+        super.touchesCancelled(touches, with: event)
+        if state == .began || state == .changed {
+            state = .cancelled
+        }
+    }
 }
 
 extension TouchObserveGesture: UIGestureRecognizerDelegate {
-  public func gestureRecognizer(
-    _ gestureRecognizer: UIGestureRecognizer,
-    shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
-  ) -> Bool {
-    return true
-  }
-
-  public func gestureRecognizer(
-    _ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer
-  ) -> Bool {
-    return false
-  }
-
-  public func gestureRecognizer(
-    _ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer
-  ) -> Bool {
-    return false
-  }
+    public func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer,
+        shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer
+    ) -> Bool {
+        return true
+    }
+    
+    public func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer, shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer
+    ) -> Bool {
+        return false
+    }
+    
+    public func gestureRecognizer(
+        _ gestureRecognizer: UIGestureRecognizer, shouldBeRequiredToFailBy otherGestureRecognizer: UIGestureRecognizer
+    ) -> Bool {
+        return false
+    }
 }
